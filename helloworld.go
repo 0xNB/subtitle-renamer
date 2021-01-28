64ebdd0a8ab590e4fff1d7ee1c538f31825fcf2a
Einstein <Zyboarg@users.noreply.github.com>
2021-01-28 21:56:32 +0100

    subtitle renamer parse properties

--- helloworld.go
+++ helloworld.go
@@ -1,8 +1,36 @@
 package main
 
-import "fmt"
-import "rsc.io/quote"
+import (
+	"flag"
+	"fmt"
+)
+
+var subtitleName string
+var showName string
+
+var auto bool
+var resizeFoundSubs bool
+var subSize int
+
+func initFlags() {
+
+	const (
+		usageSubs = "Use this option to define the common subtitle name to rename"
+	)
+
+	flag.StringVar(&subtitleName, "subname", "", usageSubs)
+	flag.StringVar(&subtitleName, "s", "", usageSubs+" (shorthand)")
+	flag.StringVar(&showName, "mkvname", "", "Specifies the name of the show to use, choose a name that most of the file names contain!")
+
+	flag.BoolVar(&auto, "auto", false, "Specifies whether the script should automatically deduce show and subtitle name from file formats, asks before renaming!")
+	flag.BoolVar(&resizeFoundSubs, "resize", false, "Use this flag to resize all found subtitles if they are in a recognized format")
+	flag.IntVar(&subSize, "subsize", 40, "Specifies the size to which the subitltes should be resized to, default value is [40]")
+}
 
 func main() {
-    fmt.Println(quote.Opt())
+	// create flags and parse them
+	initFlags()
+	flag.Parse()
+
+	fmt.Println("Given value for subtitle name is", subtitleName)
 }
