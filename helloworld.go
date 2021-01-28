b9c80a61b2df1d4f3663027da221c8ac39785f70
Einstein <Zyboarg@users.noreply.github.com>
2021-01-28 21:59:25 +0100

    formatting and comments

--- helloworld.go
+++ helloworld.go
@@ -5,9 +5,11 @@ import (
 	"fmt"
 )
 
+/*
+* Name of the subtile file that we want to find
+ */
 var subtitleName string
 var showName string
-
 var auto bool
 var resizeFoundSubs bool
 var subSize int
@@ -21,7 +23,6 @@ func initFlags() {
 	flag.StringVar(&subtitleName, "subname", "", usageSubs)
 	flag.StringVar(&subtitleName, "s", "", usageSubs+" (shorthand)")
 	flag.StringVar(&showName, "mkvname", "", "Specifies the name of the show to use, choose a name that most of the file names contain!")
-
 	flag.BoolVar(&auto, "auto", false, "Specifies whether the script should automatically deduce show and subtitle name from file formats, asks before renaming!")
 	flag.BoolVar(&resizeFoundSubs, "resize", false, "Use this flag to resize all found subtitles if they are in a recognized format")
 	flag.IntVar(&subSize, "subsize", 40, "Specifies the size to which the subitltes should be resized to, default value is [40]")
@@ -32,5 +33,6 @@ func main() {
 	initFlags()
 	flag.Parse()
 
+	// print status in console
 	fmt.Println("Given value for subtitle name is", subtitleName)
 }
