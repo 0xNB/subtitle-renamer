b2d74b51892ccdd124d52cc2f91c74c863e20ddf
Einstein <Zyboarg@users.noreply.github.com>
2021-01-29 01:02:02 +0100

    main

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 * Name of the subtile file that we want to find
  */
 var subtitleName string
-var showName string
+var vidName string
 var auto bool
 var resizeFoundSubs bool
 var subSize int
@@ -17,15 +17,24 @@ var subSize int
 func initFlags() {
 
 	const (
-		usageSubs = "Use this option to define the common subtitle name to rename"
+		usageSubs    = "Use this option to define the common subtitle name to rename"
+		usageVidName = "Specifies the name of the show to use, choose a name that most of the file names contain!"
+		usageAuto    = "Specifies whether the script should automatically deduce show and subtitle name from file formats, asks before renaming! Default is [false]"
+		usageResize  = "Use this flag to resize all found subtitles if they are in a recognized format"
+		usageSubSize = "Specifies the size to which the subitltes should be resized to, default value is [40]"
 	)
 
 	flag.StringVar(&subtitleName, "subname", "", usageSubs)
 	flag.StringVar(&subtitleName, "s", "", usageSubs+" (shorthand)")
-	flag.StringVar(&showName, "mkvname", "", "Specifies the name of the show to use, choose a name that most of the file names contain!")
-	flag.BoolVar(&auto, "auto", false, "Specifies whether the script should automatically deduce show and subtitle name from file formats, asks before renaming!")
-	flag.BoolVar(&resizeFoundSubs, "resize", false, "Use this flag to resize all found subtitles if they are in a recognized format")
-	flag.IntVar(&subSize, "subsize", 40, "Specifies the size to which the subitltes should be resized to, default value is [40]")
+	flag.StringVar(&vidName, "vidname", "", usageVidName)
+	flag.StringVar(&vidName, "v", "", usageVidName+" (shorthand)")
+	flag.BoolVar(&auto, "auto", false, usageAuto)
+	flag.BoolVar(&auto, "a", false, usageAuto+" (shorthand)")
+	flag.BoolVar(&resizeFoundSubs, "resize", false, usageResize)
+	flag.BoolVar(&resizeFoundSubs, "r", false, usageResize+" (shorthand)")
+	flag.IntVar(&subSize, "subsize", 40, usageSubSize)
+	flag.IntVar(&subSize, "z", 40, usageSubSize+" (shorthand)")
+
 }
 
 func main() {
